7959e9508ab1a9d9f2a0cf7f8b40aae50abde8a9
YMP-BLACRUSH <67401418+YMP-BLACRUSH@users.noreply.github.com>
2022-01-08 20:58:49 +0700

    Fetching Menu Products

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
+import React, { Component } from 'react'
 import { Col, Container, Row } from "react-bootstrap";
-import { Hasil, ListCategories, NavbarComponent } from "./components";
+import { Hasil, ListCategories, NavbarComponent, Menus } from "./components";
+import { API_URL } from "./utils/constants";
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <NavbarComponent />
-      <div className="mt-3">
-        <Container fluid>
-          <Row>
-            <ListCategories />
-            <Col>
-              <h4><strong>Daftar Produk</strong></h4>
-              <hr />
-            </Col>
-            <Hasil />
-          </Row>
-        </Container>
+export default class App extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      menus: [],
+    }
+  }
+
+  componentDidMount() {
+    axios.get(API_URL + '/products')
+      .then(res => {
+        const menus = res.data;
+        this.setState({ menus });
+      })
+      .catch(err => {
+        console.log(err);
+        
+      })
+  }
+
+  render() { 
+
+    const { menus } = this.state;
+
+    return (
+      <div className="App">
+        <NavbarComponent />
+        <div className="mt-3">
+          <Container fluid>
+            <Row>
+              <ListCategories />
+              <Col>
+                <h4><strong>Daftar Produk</strong></h4>
+                <hr />
+                <Row>
+                  {menus && menus.map(menu => (
+                    <Menus 
+                      key={menu.id}
+                      menu={menu}
+                    />
+                  ))}
+                </Row>
+              </Col>
+              <Hasil />
+            </Row>
+          </Container>
+        </div>
       </div>
-    </div>
-  );
+    )
+  }
 }
 
-export default App;
---
+++ src/components/Menus.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Button, Card, Col } from 'react-bootstrap'
+import numberWithCommas from '../utils/utils'
+
+const Menus = ({ menu }) => {
+    return (
+        <Col md={4} xs={6} className='mb-4'>
+            <Card className="shadow">
+                <Card.Img variant="top" src={"assets/images/"+menu.category.nama.toLowerCase()+"/"+menu.gambar} />
+                <Card.Body>
+                    <Card.Title>{menu.nama} <strong>({menu.kode})</strong></Card.Title>
+                    <Card.Text>
+                        Rp. {numberWithCommas(menu.harga)}
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+            </Card>
+        </Col>
+    )
+}
+
+export default Menus
